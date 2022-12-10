be200464776cd22459826bc52e333486a951f2f1
Nikita Portnoy <Nikita.Portnoy.Tech@gmail.com>
2022-12-09 21:07:00 -0800

    >API: Added JWT authentication with premium endpoint decorator

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify, abort
+from flask import Flask
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
+    jwt = JWTManager(app)
 
     app.config.from_pyfile('../config_dev.cfg')
 
     from app.api import API
     app.register_blueprint(API)
+
+    #JWT Identity Loader
+    @jwt.user_identity_loader
+    def jwt_identity(user):
+        return user.public_id
 
     return app
--- api/app/api.py
+++ api/app/api.py
@@ -1,6 +1,41 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
+from datetime import datetime, timezone, timedelta
+from flask_jwt_extended import get_jwt, create_access_token, get_jwt_identity, set_access_cookies, verify_jwt_in_request, jwt_required
+from functools import wraps
+
+from app.model.user.user import User
 
 API = Blueprint('API', __name__)
+
+def premium_only(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        jwt_claims = get_jwt()
+        if not jwt_claims['is_premium']:
+            abort(403)
+
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@API.after_request
+def refresh_jwt(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now_timestamp = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now_timestamp + timedelta(minutes=15))
+
+        if target_timestamp > exp_timestamp:
+            jwt_identity = get_jwt_identity()
+            account = User(public_id=jwt_identity)
+            
+            access_token = create_access_token(identity=account, additional_claims={"is_premium": account.is_premium})
+            set_access_cookies(response, access_token)
+            
+            return response
+    except (RuntimeError, KeyError):
+        return response
 
 @API.route("/", methods=['GET'])
 def route_root():
@@ -9,11 +44,37 @@ def route_root():
 
 @API.route("/user/register", methods = ['POST'])
 def route_user_register():
-    pass
+    request_data = request.json()
+
+    # TODO Data Validation
+    # TODO Duplicate Check
+
+    try:
+        User.new_user(request_data['first_name'], request_data['last_name'], request_data['email'], request_data['password'])
+        account = User(email=request_data['email'])
+        account_dict = account.to_dict()
+
+        return jsonify({'user': account_dict}), 200
+    except Exception as e:
+        return jsonify({'error': 'Request Failed', 'meessage': str(e)}), 400
 
 @API.route("/user/login", methods = ['POST'])
 def route_user_login():
-    pass
+    request_data = request.json()
+
+    try:
+        account = User(email=request_data['email'])
+        assert account.integrity, "Invalid Credentials"
+        account_dict = account.to_dict()
+
+        response = jsonify({'user': account_dict})
+
+        access_token = create_access_token(identity=account, additional_claims={"is_premium": account.is_premium})
+        set_access_cookies(response, access_token)
+
+        return response, 200
+    except Exception as e:
+        return jsonify({'error': 'Request Failed', 'meessage': str(e)}), 400
 
 @API.route("/checkouts/new", methods = ['GET'])
 def route_checkouts_new():
@@ -23,9 +84,8 @@ def route_checkouts_new():
 def route_checkouts():
     request_data = request.json()
 
-
-# Protected
 @API.route("/statistics", methods = ['GET'])
+@jwt_required()
 def route_statistics():
     
     return jsonify({'statistics': [
@@ -37,6 +97,7 @@ def route_statistics():
     ]})
 
 @API.route("/premium/statistics", methods = ['GET'])
+@premium_only
 def route_premium_statistics():
     
     return jsonify({'statistics': [
